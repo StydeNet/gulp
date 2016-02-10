cecf5a2f3cf7a8a1f05a5f6276fda549aabbfcd2
Ramon Lapenta <ramono@gmail.com>
2016-02-10 20:15:28 +0000

    Introducción a Gulp
    Código como aparece al final del primer video.

---
+++ Gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    watch = require('gulp-watch');
+
+var config = {
+    scssDir: './assets/scss',
+    cssDir: './assets/css'
+};
+
+gulp.task('style', function(){
+    gulp.src(config.scssDir + '/*.scss')
+    .pipe(sass())
+    .on('error', sass.logError)
+    .pipe(gulp.dest(config.cssDir))
+});
+
+gulp.task('watch', function(){
+    watch(config.scssDir + '/**/*.scss', function(){
+        gulp.start('style');
+    });
+});
